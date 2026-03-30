3150570d196d6a35f381c3924a002b942db3d4d3
Josemar Pascoal <josemarpas26@gmail.com>
2026-03-31 00:53:01 +0200

    Publocando o email. no entry.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,13 @@ set -e
 
 PORT=${PORT:-8000}
 
-
-# --force ignora a confirmação de produção
+php artisan vendor:publish --tag=laravel-mail --force
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
 php artisan route:clear
 
 php artisan migrate --force --no-interaction
-# php artisan db:seed --force
 
 php artisan storage:link --force 2>/dev/null || true
 
